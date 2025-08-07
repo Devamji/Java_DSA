5e4d380bfa04df003847b84399be3cb92693d689
Devamji <115780345+Devamji@users.noreply.github.com>
2025-08-07 22:57:29 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -1,10 +1,16 @@
 package Basic;
 import java.util.*;
 
-// temp , a , b 
-//temp <= a
-// a <= b
-// b <= temp
+// this program we first make (temp , a , b) three variable
+// temp is a empty shell or a box and a,b contain a value.
+//we put "a " value in "temp" box (temp <= a)
+// then we transfer b value in "a" box (a <= b)
+// then we transfer tamp value in "b" box b <= temp
+
+// then we observe that a's value is moved in temp box (a box become empty) and b's value is moved in a's box (b box are now empty) then temp value moved in b's box (now temp is empty)
+// now a's value become b's and b's value become a's.
+// by the help of this process we successfully swap that value. 
+
 
 public class swap {
     public static void swapno(int a , int b) {
@@ -23,5 +29,6 @@ public class swap {
         swapno(a, b);
     }
 }
+
 
 
