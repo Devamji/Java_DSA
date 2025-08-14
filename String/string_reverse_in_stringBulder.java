31abd8ea35286f60acbbe452eff3efda237733ec
Devamji <115780345+Devamji@users.noreply.github.com>
2025-08-14 21:18:28 +0530

    Update string_reverse_in_stringBulder.java

--- String/string_reverse_in_stringBulder.java
+++ String/string_reverse_in_stringBulder.java
@@ -3,9 +3,9 @@ package String;
 public class string_reverse_in_stringBulder {
         public static void main(String[] args) {
             StringBuilder sb = new StringBuilder("hello");
-            for (int i=0; i<sb.length()/2; i++){
+            for (int i=0; i<sb.length()/2; i++){              // string ko half mai divided kar denge 
                 int front = i;
-                int back = sb.length() -1 - i;
+                int back = sb.length() -1 - i;                // dono part ko reverse kar arhe hai. 
 
                 char frontChar = sb.charAt(front);
                 char backChar = sb.charAt(back);
@@ -17,3 +17,4 @@ public class string_reverse_in_stringBulder {
             System.out.println(sb);
         }
 }
+
