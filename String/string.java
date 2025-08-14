1b16441a6b5c939354c03ebfdd8354f8ff5919d7
Devamji <115780345+Devamji@users.noreply.github.com>
2025-08-14 15:51:58 +0530

    Update string.java

--- String/string.java
+++ String/string.java
@@ -47,5 +47,45 @@ public class string {
         //return true if given string is equal or not --> it case senstive 
        
         System.out.println(name.equals("enter the string to comparision::"+sc.next()));
+
+        System.out.println(str.equalsIgnoreCase(str2));
+
+        String m = "a";//ASIC value is 97
+        String n = "A";// ASIC value is 65    // isko ye first convert karega ASIC value mai then compair karega ( 97 - 65)
+        // in this case if 0 come then m = n 
+        // if positive value come then m > n
+        // if negative value come then m < n
+        System.out.println(m.compareTo(n));
+
+        //indexOf() mathord is used to give the index position of the character or string 
+        System.out.println(str.indexOf("v"));
+
+        // lastIndexOf() mathord is use to find the index from the last element 
+        String str3 = "deepak";
+        System.out.println(str3.lastIndexOf("e")); 
+        System.out.println(str3.indexOf("e"));
+
+        //contains() methord is use to shearch the element in string 
+        System.out.println(str.contains("a"));
+
+        //charAt() matherd is used to find the element or characher 
+        System.out.println(str.charAt(3)); // it ask index 3 par kya hai.
+    
+        //Cancatination are use to mearge the two different string or characters in one string
+        String a= "Devam";
+        String b = "mishra";
+        System.out.println(a.concat(b));
+
+        //isEmpty() meathord is use to see the given string are empty or not
+        String o = "Hello";
+        String p = "";
+        System.out.println(o.isEmpty());
+        System.out.println(p.isEmpty());
+
+        //join() meathord is use to join the multiple string or character in one string.
+        // delimiter is use to seprate the element. 
+        String name1 = String.join(" ", "Devam", "Mishra", "age", "is","20");
+        System.out.println(name1);
     }  
 }
+
