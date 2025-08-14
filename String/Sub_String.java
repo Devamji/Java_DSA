7c7bcf6377936d94ba963f7f88edfb1e0445d04c
Devamji <115780345+Devamji@users.noreply.github.com>
2025-08-14 20:59:22 +0530

    Update Sub_String.java

--- String/Sub_String.java
+++ String/Sub_String.java
@@ -1,10 +1,15 @@
 package String;
 import java.util.*;
+
+// string sub = " "
+// for (i = si; i< ei ; i++)
+//     substr += str.charAt(i);
+
 public class Sub_String {
     public static String substring(String str , int si , int ei){
-        String substr =" ";
-        for (int i=si; i<ei ; i++){
-            substr += str.charAt(i);
+        String substr =" ";                         // make empty string
+        for (int i=si; i<ei ; i++){                 // si se le ke ei-1 tak chelega
+            substr += str.charAt(i);                // String ke i number index ka character deta hai
         }
         return substr;
     }
@@ -16,4 +21,5 @@ public class Sub_String {
         System.out.println(str.substring(0,5));
     }
 }
-// the is string
+
+
