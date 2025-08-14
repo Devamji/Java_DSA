6861f1fec06cff102a7f5473201d3f428155652f
Devamji <115780345+Devamji@users.noreply.github.com>
2025-08-14 21:09:10 +0530

    Update pal.java

--- String/pal.java
+++ String/pal.java
@@ -2,11 +2,16 @@ package String;
 import java.util.*;
 import java.util.Scanner;
 
+// for (i = 0 ; i < a.length/2 ; i++)
+// if (a.carAt(i) != a.charAt(n-1-i)
+//  return false
+
+
 public class pal {
     public static boolean palindrome(String a) {
-        int n = a.length();
-        for (int i = 0; i < n / 2; i++) {
-            if (a.charAt(i) != a.charAt(n - 1 - i)) {
+        int n = a.length();                            // isme hum length find karte hai
+        for (int i = 0; i < n / 2; i++) {              // hum isme string ko half se brack kar rahe hai 
+            if (a.charAt(i) != a.charAt(n - 1 - i)) {  // a.charAt(i) : string ka left side se i-th character le raha hai and a.charAt(n - 1 - i): string ka right side se i-th character le raha hai
                 return false;
             }
         }
@@ -20,4 +25,5 @@ public class pal {
         System.out.println("Is palindrome? " + palindrome(a));
     }
 }
+
 
