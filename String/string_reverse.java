cab26101fa85f41e2123c0cc7b5d53362188ff80
Devamji <115780345+Devamji@users.noreply.github.com>
2025-08-27 14:09:10 +0530

    Update string_reverse.java

--- String/string_reverse.java
+++ String/string_reverse.java
@@ -1,25 +1,6 @@
 package String;
 
 public class string_reverse {
-        // public static void main(String[] args) {
-        //     StringBuilder sb = new StringBuilder("hello");
-        //     for (int i=0; i<sb.length()/2; i++){
-        //         int front = i;
-        //         int back = sb.length() -1 - i;
-
-        //         char frontChar = sb.charAt(front);
-        //         char backChar = sb.charAt(back);
-
-        //         sb.setCharAt(front,   backChar);
-        //         sb.setCharAt(back, frontChar);
-        //     }
-
-        //     System.out.println(sb);
-        // }
-
-   
-    
-    
     public static String reverse(String str) {
         String reversed = "";                           
         for (int i = str.length() - 1; i >= 0; i--) {
@@ -41,4 +22,5 @@ public class string_reverse {
 
 
    
+
 
